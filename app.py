c7ca5a4e8681d75f57903aef3f6837e80345a7d4
aaditya-patil-0018 <ampatil.6204@gmail.com>
2021-03-25 22:06:18 +0530

    Completed most of the features of the Thaku's Website

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 import time
+import csv
 
 app = Flask(__name__)
 
+data = []
+db = None
 @app.route('/')
 def index():
     today = datetime.today()
     today = today.strftime("%d/%m/%Y")
     now = datetime.now()
     now = now.strftime("%H:%M:%S")
-    while True:
-        return render_template('index.html', today=today, now=now)
+    for d in data:
+        if d[1] != today:
+            data.remove(d)
+    return render_template('index.html', today=today, now=now, data=data, db=db)
+
+@app.route('/', methods=["POST"])
+def info():
+    name = request.form.get("name")
+    today = datetime.today()
+    today = today.strftime("%d/%m/%Y")
+    now = datetime.now()
+    now = now.strftime("%H:%M:%S")
+    li = [name, today, now]
+    data.append(li)
+    return index()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
