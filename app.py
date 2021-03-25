f2f617b9a9c1df3d93942f0d138f221c75b98e21
Aadi <ampatil.6204@gmail.com>
2021-03-25 19:16:03 +0530

    Corrected some change from laptop

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import time
 
 app = Flask(__name__)
 
@@ -8,8 +9,8 @@ def index():
     today = datetime.today()
     today = today.strftime("%d/%m/%Y")
     now = datetime.now()
+    now = now.strftime("%H:%M:%S")
     while True:
-        now = now.strftime("%H:%M:%S")
         return render_template('index.html', today=today, now=now)
 
 if __name__ == "__main__":
